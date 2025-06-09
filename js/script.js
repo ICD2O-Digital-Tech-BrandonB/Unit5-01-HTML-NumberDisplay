69dd71553b4a9d6d98908e8fb9482ccf2c086abd
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-06-09 15:05:14 +0000

    501

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict";
+
+function intakeNumber() {
+
+    let counter = 0
+
+    let numbers = ""
+
+    let minNumb = parseInt(document.getElementById("minNumber").value)
+    let maxNumb = parseInt(document.getElementById("maxNumber").value)
+
+    while (counter <= maxNumb) {
+        if (counter >= minNumb) {
+            numbers += counter + "<br>"
+        }
+        counter = counter + 1
+    }
+
+    document.getElementById("outputNumber").innerHTML = numbers
+}
